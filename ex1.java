65fd40b61f4a4e35c83eeabca35a2ef7f90d2eff
raialucas1 <105650085+raialucas1@users.noreply.github.com>
2023-11-24 12:17:39 -0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+import javax.swing.text.StyleConstants.CharacterConstants;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        /*IF/ELSE
+        EXERCICIO 1 
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Digite seu email:");
+        String email=sc.nextLine();
+        System.out.println("Digite sua senha:");
+        String senha=sc.nextLine();
+
+        String email2="raialucas@gmail.com";
+        String senhaPadrao="1234";
+        if(email.equals(email2)&& senha.equals(senhaPadrao)){
+            System.out.println("login concedido!");
+        }else{
+            System.out.println("Email ou senha inválidos!");
+        }
+        sc.close();*/
+
+
+        /* EXERCICIO 2
+         Scanner sc=new Scanner(System.in);
+        System.out.println("Digite um número:");
+        int a=sc.nextInt();
+        System.out.println("Digite outro número:");
+        int b=sc.nextInt();
+        System.out.println("A soma dos números é:"+(a+b));
+        sc.close();*/
+
+        
+   
+         /*EXERCICIO 3 
+          * 
+          System.out.println("Digite qualquer coisa:");
+            String txt=sc.nextLine();
+          
+            for (char caractere: txt.toCharArray()) {
+                if(Character.isDigit(caractere)){
+                    System.out.println("Existe um número na digitação");
+                }
+            }
+
+
+
+
+         */
+
+         /*EXERCICIO 4 
+          * 
+           System.out.println("Digite sua idade:");
+        int idade=sc.nextInt();
+
+        if(idade>18){
+            System.out.println("Você é um adulto");
+        }else{
+            System.out.println("Você não é um adulto!");
+        }
+
+
+         */
+        /*EXERCICIO 5
+          int alturaMax=10;
+
+        System.out.println("Qual a altura do pacote:");
+        int altura=sc.nextInt();
+
+        if(altura>alturaMax){
+            System.out.println("Altura máxima excedida!");
+        }else{
+            System.out.println("tamanho do pacote válido!");
+        }
+       
+                */
+       
+            
+        
+        
+        
+    }
+}
